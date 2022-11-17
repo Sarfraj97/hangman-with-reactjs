93c7894ef6cc82d8dfcf6e14ad4335f9b5ad7d43
Sarfraz Khan <szkkhan2@gmail.com>
2022-11-17 15:25:33 +0530

    adding key in WrongAttempt map

--- src/components/WrongAttempt.js
+++ src/components/WrongAttempt.js
@@ -4,7 +4,7 @@ export default function WrongAttempt({ wrongAttempts }) {
   return (
     <div>WrongAttempt 
       {wrongAttempts.map((word, i) => {
-        return <div><p className="answerSpace" key={i}>{word}</p></div>
+        return <p className="answerSpace" key={word.toString()}>{word}</p>
       })}
     </div>
 
